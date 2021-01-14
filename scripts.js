b16c8f8bf2bde1ffed814a78bdac7091bbfd20d4
Askhab <asmag86@gmail.com>
2021-01-14 14:30:51 +0300

    Added checks for filling in answers

--- scripts.js
+++ scripts.js
@@ -14,7 +14,23 @@ for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов', ''),
           b = prompt('На сколько оцените его?', '');
 
-    personalMoviesDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMoviesDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.error('error');
+        i--;
+    }
+}
+
+if (personalMoviesDB.count < 10) {
+    console.log(`Few flms watched!`);
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB <= 30) {
+    console.log(`You're classical cinemafil!`);
+} else if (personalMoviesDB.count >= 30) {
+    console.log(`You're supercinemalover!`);
+} else {
+    console.log(`An error occused!!!`);
 }
 
 console.log(personalMoviesDB);
